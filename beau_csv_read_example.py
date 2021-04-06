c10de022ddea4df46478acb8845a640eed71cf97
Beau Butler <beau.butler@gmail.com>
2021-04-06 15:29:26 +1200

    Fixed a bunch of syntax glitches

--- beau_csv_read_example.py
+++ beau_csv_read_example.py
@@ -1,28 +1,30 @@
 
-
-import zipfile, csv
+import zipfile
+import csv
 from pprint import pprint
 
 
-def CMA(n):  return ','.join(textwrap.wrap(str(n)[::-1],3))[::-1]
+def CMA(n):  
+    return ','.join(textwrap.wrap(str(n)[::-1],3))[::-1]
+
+
 # Pull from the exactbins zipfile directly.
 
 def main():
-    INFILE = 'exactbins.zip'    ;   INCSV = 'exactbins.csv'
-    print 'Loading zipfile'
-    if not zipfile.is_zipfile(INFILE):  return 'zipfile '+INFILE+' bad or not found'
+    INFILE = 'exactbins.zip'    
+    INCSV = 'exactbins.csv'
+    print('Loading zipfile')
+    if not zipfile.is_zipfile(INFILE):  
+        print('zipfile '+INFILE+' bad or not found')
+        return
     
     csvLines = zipfile.ZipFile(INFILE).read(INCSV).splitlines()
 
     reader = csv.reader(csvLines)
 
     for row in reader:
-    	print row
+        print(row)
 
 if __name__ == '__main__':
-	main()
-
-	
-    
-
+    main()
 
